baa0cbb73a0fdfa383c10f37cbad62f6915fa796
locke <locke@LockeThinkpad>
2018-02-10 14:06:35 -0500

    Didn't stage changes last time so here it is again. Reformatted/ reorganized code and renamed lots of stuff. Added elevator pid command, changed assignments and various other small things.

---
+++ 2018_PowerUp/src/org/usfirst/frc/team4188/robot/commands/elevator/ElevatorToSwitch.java
@@ -0,0 +1,50 @@
+package org.usfirst.frc.team4188.robot.commands.elevator;
+
+import org.usfirst.frc.team4188.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class ElevatorToSwitch extends Command {
+
+    private double setpoint;
+	private double tolerance;
+	
+	public ElevatorToSwitch(double setpoint, double tolerance) {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	this.setpoint = setpoint;
+    	this.tolerance = tolerance;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.m_elevator.setSetpoint(setpoint);
+    	Robot.m_elevator.setAbsoluteTolerance(tolerance);
+    	Robot.m_elevator.setPID(.25, .1, 0);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.m_elevator.enable();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.m_elevator.onTarget();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.m_elevator.disable();
+    	Robot.m_elevator.free();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+    
+}
